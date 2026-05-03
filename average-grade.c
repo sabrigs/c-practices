fbcdfca9d36b7226f9470c255af013a678f5e383
sabrigs <sabrinagsilva@proton.me>
2026-05-03 14:52:16 -0300

    Add new exercise about average

---
+++ average-grade.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+// Global variables
+int sum_grade = 0;
+int grades = 0;
+
+// Functions prototypes
+void ask_grade(int i);
+float cal_average(void);
+
+int main(void)
+{
+    printf("How many grades? ");
+    scanf("%i", &grades);
+
+    for(int i = 1; i <= grades; i++)
+    {
+        ask_grade(i);
+    }
+    
+    return cal_average();
+}
+
+// Functions
+// Ask grade and store the results
+void ask_grade(int i)
+{
+    float grade;
+    printf("✦ Grade %i: ", i);
+    scanf("%f", &grade);
+    sum_grade = sum_grade + grade;
+}
+
+// Calculate grades average
+float cal_average(void)
+{
+    float average = (float) sum_grade / grades;
+    printf("Grade's average: %.2f\n", average);
+    return 0;
+}
